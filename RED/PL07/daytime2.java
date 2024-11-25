22376fb221166a310e6bb563bc27ab3be03ba0de
Gabriela <146675101+gabrielajamhour@users.noreply.github.com>
2024-11-25 22:13:14 +0100

    Add files via upload

---
+++ RED/PL07/daytime2.java
@@ -0,0 +1,35 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Date;
+
+public class daytime2 {
+    public static void main(String[] args) {
+
+        try {
+            DatagramSocket ds = new DatagramSocket(7777);
+            
+            ds.setSoTimeout(5000);
+
+            while (true) {
+                byte[] b = new byte[1000];
+                DatagramPacket p = new DatagramPacket(b, 1000);
+
+                ds.receive(p);
+
+                Date now = new Date();
+                String now_string = now.toString() + "\r\n";
+                
+                DatagramPacket dp = new DatagramPacket(
+now_string.getBytes(), now_string.getBytes().length, 
+p.getAddress(), 
+p.getPort()
+);
+
+                ds.send(dp);
+                System.out.println(now_string);
+            }
+        } catch (Exception e) {
+ System.out.println(e);
+ }
+    }
+}
