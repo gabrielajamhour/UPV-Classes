cf927e939e0da36e0b6ea46aba5f1fb57687c01f
Gabriela <146675101+gabrielajamhour@users.noreply.github.com>
2024-11-25 22:24:52 +0100

    Add files via upload

---
+++ FSO/PL04/adopted_process.c
@@ -0,0 +1,19 @@
+/* adopted_process.c */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Proceso padre %ld\n", (long)getpid());
+    int val_return = 0;
+    for(int i = 0; i < 5; i++){
+        val_return = fork();
+        if (val_return == 0) {
+            printf("Hijo creado en iteración = %d\n", i);
+            sleep(20);
+            exit(i);
+        }
+    }
+    sleep(10);
+    exit(0);
+}
