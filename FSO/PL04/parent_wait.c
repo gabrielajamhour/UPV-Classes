e95a1042f653aaa30a5b9be1865ba624e31ba010
Gabriela <146675101+gabrielajamhour@users.noreply.github.com>
2024-11-25 22:26:16 +0100

    Add files via upload

---
+++ FSO/PL04/parent_wait.c
@@ -0,0 +1,19 @@
+/* parent_wait.c */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Proceso padre %ld\n", (long)getpid());
+    int val_return = 0;
+    for(int i = 0; i < 5; i++){
+        val_return = fork();
+        if (val_return == 0) {
+            printf("Hijo creado en iteración = %d\n", i);
+            exit(i);
+        }
+        wait();
+    }
+    sleep(10);
+    exit(0);
+}
